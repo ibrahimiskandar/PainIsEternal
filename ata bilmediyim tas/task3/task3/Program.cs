10b012d5d4d090b06ef1bae1c381c6fba8c21530
ibrahimiskandar <100687451+ibrahimiskandar@users.noreply.github.com>
2022-04-11 14:00:16 +0400

    sorry(

---
+++ ata bilmediyim tas/task3/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Pizza Mozzarella Rera Rera Rera";
+
+            RvrsText(text);
+        }
+        static void RvrsText(string text)
+        {
+            StringBuilder newtext;
+            int lastspace = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    lastspace = i;
+                }
+
+            }
+            for (int j = lastspace+1; j < text.Length; j++)
+            {
+                newtext.Append(text[j]);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
+
+
+
